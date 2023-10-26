5849315fb085c64867b3c9053b554a16219d609f
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2023-10-26 19:45:34 +0530

    Update first.py

--- first.py
+++ first.py
@@ -29,7 +29,7 @@ def index():
         u = y.split(',')
         for i in range(0, len(u)):
             u[i] = u[i].strip()
-        return render_template ('result1.html',valid=u[0],number=u[1],local=u[2],inter=u[3],pref=u[4],code=u[5],name=u[6],loc=u[7],carr=u[8],type=u[9],
+        return render_template ('result.html',valid=u[0],number=u[1],local=u[2],inter=u[3],pref=u[4],code=u[5],name=u[6],loc=u[7],carr=u[8],type=u[9],
 )
     else:
         return render_template('index.html')
